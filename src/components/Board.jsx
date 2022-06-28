08cf990518838e09dd19c31461493645ba849431
Aditya_Kumar_Ray <rayaditya414@gmail.com>
2022-06-28 22:21:26 +0530

    created the boards layout

---
+++ src/components/Board.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Sqaure from './Sqaure'
+
+const board = () => {
+  return (
+    <div>
+        <div>
+            <Sqaure value={0}/>
+            <Sqaure  value={1}/>
+            <Sqaure  value={2}/>
+        </div>
+        <div>
+            <Sqaure  value={3}/>
+            <Sqaure  value={4}/>
+            <Sqaure  value={5}/>
+        </div>
+        <div>
+            <Sqaure  value={6}/>
+            <Sqaure  value={7}/>
+            <Sqaure value={8}/>
+        </div>
+
+    </div>
+  );
+};
+
+export default board
---
+++ src/components/Sqaure.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sqaure = ({value}) => {
+  return (
+    <button type="button">{value}</button>
+  );
+}
+
+export default Sqaure
