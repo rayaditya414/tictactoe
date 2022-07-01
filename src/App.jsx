c77e8fb0d079f41634f52f024be23a24b1e1d394
Aditya_Kumar_Ray <rayaditya414@gmail.com>
2022-07-02 02:05:22 +0530

    History Created and kept Track of

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,47 @@ import "./styles/root.scss"
 const App=()=>{
 
 
-  const [board,setBoard]=useState(Array(9).fill(null));
-  const[isNext,setIsNext]=useState(false);
+  const [history,setHistory]=useState([
+  { board:Array(9).fill(null), isNext:true}
+  ]);
+  const[currentMove,setCurrentMove]=useState(0);
 
-  const winner=calculateWinner(board);
+  const current=history[currentMove];
+  console.log(history);
+
+  const winner=calculateWinner(current.board);
   const message=winner
         ?`Winner is ${winner}`:
-        `Next player is ${isNext?'X':'O'}`;
+        `Next player is ${current.isNext?'X':'O'}`;
 
 
 
   const handleSqaureClick =(position)=>{
-    if(board[position]!=null || winner) return;
-    setBoard((prev)=>{
-      return prev.map((square,pos)=>{
+    if(current.board[position]!=null || winner) return;
+
+
+    setHistory((prev)=>{
+
+      const last=prev[prev.length -1];
+
+      const newBoard=last.board.map((square,pos)=>{
         if(pos==position) {
-          return isNext?'X':'O';
+          return last.isNext?'X':'O';
         }
         return square;
       });
+
+      return prev.concat({ board:newBoard ,isNext:!last.isNext});
      
     })
-    setIsNext((prev)=> !prev);
+    setCurrentMove((prev)=> prev+1);
   }
 
   return(
   <div className="app">
   <h1>TIC TAC TOE!</h1>
   <h2>{message}</h2>
-  <Board board={board} handleSqaureClick={handleSqaureClick}/>
+<Board board={current.board} handleSqaureClick={handleSqaureClick}/>
 </div>);
 }
 
